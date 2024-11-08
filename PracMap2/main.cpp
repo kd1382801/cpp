3203ab370b8f6b164d02746900089f05bec3a0b1
kd1382801 <kd1382801@st.kobedenshi.ac.jp>
2024-11-08 14:24:34 +0900

    20241108

--- PracMap2/main.cpp
+++ PracMap2/main.cpp
@@ -13,9 +13,34 @@ int main() {
 							{"温度","temperature"}};
 
 	auto itr = dic.begin();//先頭要素のイテレータを取得
-	for (const auto& d : dic) {
+	cout << itr->first << endl;
+
+	//範囲for
+	for (const auto d : dic) {
 		cout << d.first << endl;
 	}
+
+	//乱数生成器を作成
+	random_device rand_dev{};
+	//乱数アルゴリズムにメルセンヌツイスターを指定
+	mt19937 rand_engine(rand_dev());
+	//0～(mapの要素数)-1を均等な確率で得る分布生成器を作成
+	uniform_int_distribution<int> dist(0, dic.size() - 1);
+
+	//cout << dist(rand_engine) << endl;
+
+	itr = dic.begin();
+	itr = next(itr, dist(rand_engine));//std::next(イテレータ,進める量)
+	cout << itr->first << "の英単語は?>" << endl;
+	string str;
+	cin >> str;
+	if (str == itr->second) {
+		cout << "正解!" << endl;
+	}
+	else {
+		cout << "不正解…" << endl;
+	}
+
 
 	return 0;
 }
